69b2918854a7125b285570e73d107f599321a629
Emil Telstad <emil.telstad@gmail.com>
2023-02-14 19:51:16 +0100

    Minor formatting.

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,16 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
--- docs/vite.config.mts
+++ docs/vite.config.mts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'npm:vite@^4.0.4'
-import react from 'npm:@vitejs/plugin-react@^3.0.1'
+import react from 'npm:@vitejs/plugin-react@^3.0.1';
+import { defineConfig } from 'npm:vite@^4.0.4';
 
-import 'npm:react@^18.2.0'
-import 'npm:react-dom@^18.2.0/client'
+import 'npm:react-dom@^18.2.0/client';
+import 'npm:react@^18.2.0';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+});
