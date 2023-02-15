5e8f7645f2cc4187614bc28ee9a0f664d04e1398
Emil Telstad <emil.telstad@gmail.com>
2023-02-16 00:43:26 +0100

    Index path is blog.

--- solution/root/urls.py
+++ solution/root/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
 
 # Setup static access and media upload.
